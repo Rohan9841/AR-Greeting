602c8b9db4505344648527e1b0a04f7ee9438e97
Rohan Maharjan <Rohan9841@gmail.com>
2018-05-28 02:21:27 -0500

    Added menu scene

---
+++ AR3/Assets/Datafiles/Scripts/ChangeScene.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+
+    public void greetingCard()
+    {
+        SceneManager.LoadScene("ARGreetingCardScene");
+    }
+
+    public void rotatingCube()
+    {
+        SceneManager.LoadScene("RotatingCube");
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game exited");
+    }
+
+    public void backButton()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+	
+}
---
+++ AR3/Assets/Datafiles/Scripts/RotateCake.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCake : MonoBehaviour {
+
+    public Vector3 rotateSpeed;
+
+	// Use this for initialization
+	void Start () {
+        rotateSpeed = new Vector3(45, 20, 50);	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(rotateSpeed * Time.deltaTime);
+	}
+}
